3fb1f9fda1b66499ad7e5a5145f0e4df946a0795
Philip de Nier <philipn@rd.bbc.co.uk>
2023-01-02 13:36:07 +0000

    Various updates to support older platform/cmake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,3 @@
-# Option to build a shared object library
-option(BUILD_SHARED_LIBS "Build using shared libraries" $<IF:MSVC:OFF:ON>)
-
 # Option to build libMXF++ testing
 # This option is ignored if BUILD_TESTING is defined and falsy
 option(LIBMXFPP_BUILD_TESTING "Build libMXF++ testing" ON)
@@ -21,12 +18,18 @@ option(LIBMXFPP_BUILD_TOOLS "Build all the tools" OFF)
 option(LIBMXFPP_BUILD_GEN_CLASSES "Build the generate classes tool" ON)
 
 if(UNIX)
+    # Option to build a shared object library
+    option(BUILD_SHARED_LIBS "Build using shared libraries" ON)
+
     # Run tests with valgrind
     option(LIBMXFPP_TEST_WITH_VALGRIND "Run tests with valgrind" OFF)
 
     # Option to build libMXF from an installed library found using pkg-config
     option(LIBMXFPP_BUILD_LIBMXF_LIB "Build libMXF from installed library" OFF)
 elseif(MSVC)
+    # Shared library currently not supported
+    set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build using shared libraries")
+
     # Option to set to use the MultiThreadedDLL runtime
     option(LIBMXFPP_SET_MSVC_RUNTIME "Enable setting MSVC runtime to MultiThreadedDLL" ON)
 endif()
